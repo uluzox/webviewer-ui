c31cc69abd7ca62654debc1aead706ebb9d5b8a7
ZhijieZhang <zhijiezhang0124@gmail.com>
2019-03-07 17:58:33 -0800

    update WarningModal to use Modal

--- src/components/WarningModal/WarningModal.js
+++ src/components/WarningModal/WarningModal.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import i18next from 'i18next';
 
+import Modal from 'components/Modal';
 import Button from 'components/Button';
 
-import getClassName from 'helpers/getClassName';
 import getPopupElements from 'helpers/getPopupElements';
 
 import actions from 'actions';
@@ -19,7 +19,6 @@ class WarningModal extends React.PureComponent {
     isOpen: PropTypes.bool,
     closeElement: PropTypes.func.isRequired,
     closeElements: PropTypes.func.isRequired,
-
     confirmBtnText: PropTypes.string,
     title: PropTypes.string,
     message: PropTypes.string,
@@ -31,72 +30,67 @@ class WarningModal extends React.PureComponent {
     super();
   }
 
-  componentDidUpdate (prevProps) {
-    if (!prevProps.isOpen && this.props.isOpen) {
-      //TODO want to do make something like'closeOtherElements', but that for latter
-        let popUpToClose = getPopupElements().filter(ele => ele !== 'warningModal'); 
-        this.props.closeElements(popUpToClose);
+  handleVisibilityChange = isVisible => {
+    if (isVisible) {
+      let popUpToClose = getPopupElements().filter(ele => ele !== 'warningModal');
+      this.props.closeElements(popUpToClose);
     }
   }
 
   onCancel = () => {
     // fire cancel event from 'componentDidUpdate'
     if (this.props.onCancel) {
-        this.props.onCancel().then(() => {
-            this.props.closeElement('warningModal');
-        });
-    } else {
+      this.props.onCancel().then(() => {
         this.props.closeElement('warningModal');
+      });
+    } else {
+      this.props.closeElement('warningModal');
     }
   }
 
   onConfirm = () => {
     this.props.onConfirm().then(() => {
-            this.props.closeElement('warningModal');
-        }
-    );
+      this.props.closeElement('warningModal');
+    });
   }
-  
-  render () {
-    const { title, message, confirmBtnText} = this.props;
 
-    if (this.props.isDisabled) {
-      return null;
-    }
+  render() {
+    const { title, message, confirmBtnText } = this.props;
 
-    const className = getClassName('Modal WarningModal', this.props);
     const label = confirmBtnText || i18next.t('action.ok');
 
     const cancelBtnText = i18next.t('action.cancel');
 
     return (
-        <div className={className}>
-            <div className="container">
-                <div className="header">{title}</div>
-                <div className="body">{message}</div>
-                <div className="footer">
-                    <Button dataElement="WarningModalClearButton" label={cancelBtnText} onClick={this.onCancel} />
-                    <Button className="warningMessageConfirm" dataElement="WarningModalSignButton" label={label} onClick={this.onConfirm} />
-                </div>
-            </div>
+      <Modal 
+        className="WarningModal" 
+        dataElement="warningModal"
+        onVisibilityChange={this.handleVisibilityChange}
+      >
+        <div className="container">
+          <div className="header">{title}</div>
+          <div className="body">{message}</div>
+          <div className="footer">
+            <Button dataElement="WarningModalClearButton" label={cancelBtnText} onClick={this.onCancel} />
+            <Button className="warningMessageConfirm" dataElement="WarningModalSignButton" label={label} onClick={this.onConfirm} />
+          </div>
         </div>
+      </Modal>
     );
   }
 }
 
 const mapStateToProps = state => ({
-    title: selectors.getWarningTitle(state) ||  '',
-    message: selectors.getWarningMessage(state),
-    onConfirm: selectors.getWarningConfirmEvent(state),
-    confirmBtnText: selectors.getWarningConfirmBtnText(state),
-    onCancel: selectors.getWarningCancelEvent(state),
-    isDisabled: selectors.isElementDisabled(state, 'warningModal'),
-    isOpen: selectors.isElementOpen(state, 'warningModal'),
+  title: selectors.getWarningTitle(state) || '',
+  message: selectors.getWarningMessage(state),
+  onConfirm: selectors.getWarningConfirmEvent(state),
+  confirmBtnText: selectors.getWarningConfirmBtnText(state),
+  onCancel: selectors.getWarningCancelEvent(state),
 });
 
 const mapDispatchToProps = {
-    closeElement: actions.closeElement,
-    closeElements: actions.closeElements
+  closeElement: actions.closeElement,
+  closeElements: actions.closeElements
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(WarningModal);
