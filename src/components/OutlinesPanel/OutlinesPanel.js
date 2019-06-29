f9c8ec993b23ed4b0c6609d963211e534fc031e6
Zhijie Zhang <zhijiezhang0124@gmail.com>
2019-06-28 18:23:15 -0700

    First try

--- src/components/OutlinesPanel/OutlinesPanel.js
+++ src/components/OutlinesPanel/OutlinesPanel.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { translate } from 'react-i18next';
@@ -11,66 +11,50 @@ import selectors from 'selectors';
 
 import './OutlinesPanel.scss';
 
-class OutlinesPanel extends React.PureComponent {
-  static propTypes = {
-    display: PropTypes.string.isRequired,
-    isDisabled: PropTypes.bool,
-    t: PropTypes.func.isRequired
+const OutlinesPanel = function(props) {
+  const {
+    t, 
+    display, 
+    isDisabled
+  } = props;
+
+  const [outlines, setOutlines] = useState([]);
+
+  useEffect(() => {
+    console.log(core.addEventListener);
+    const updateOutlines = () => {
+      core.getOutlines().then(setOutlines);
+    };
+
+    core.addEventListener('documentLoaded', updateOutlines);
+    return core.removeEventListener('documentLoaded', updateOutlines);
+  }, []);
+
+  useEffect(() => {
+    const resetOutlines = () => {
+      setOutlines([]);
+    };
+
+    core.addEventListener('documentUnloaded', resetOutlines);
+    return core.removeEventListener('documentUnloaded', resetOutlines);
+  }, []);
+
+  if (isDisabled) {
+    return null;
   }
 
-  state = {
-    outlines: []
-  }  
-
-  componentDidMount() {
-    core.addEventListener('documentLoaded', this.updateOutlines);
-    core.addEventListener('documentUnloaded', this.resetOutlines);
-  }
-
-  componentWillUnmount() {
-    core.removeEventListener('documentLoaded', this.updateOutlines);
-    core.removeEventListener('documentUnloaded', this.resetOutlines);
-  }
-
-  updateOutlines = () => {
-    core.getOutlines().then(outlines => {
-      this.setState({ outlines });
-    });
-  }
-
-  resetOutlines = () => {
-    this.setState({
-      outlines: []
-    });
-  }
-
-  render() {
-    const { 
-      isDisabled, 
-      t, 
-      display 
-    } = this.props;
-    const { outlines } = this.state;
-
-    if (isDisabled) {
-      return null;
-    }
-
-    const className = getClassName('Panel OutlinesPanel', this.props);
-
-    return (
-      <div className={className} style={{ display }} data-element="outlinesPanel">
-        {outlines.length === 0 &&
-          <div className="no-outlines">{t('message.noOutlines')}</div>
-        }
-        {outlines.map((outline, i) => (
-          <Outline key={i} outline={outline} isVisible />
-        ))}
-      </div>
-    );
-    
-  }
-}
+  const className = getClassName('Panel OutlinesPanel', props);
+  return (
+    <div className={className} style={{ display }} data-element="outlinesPanel">
+      {outlines.length === 0 &&
+        <div className="no-outlines">{t('message.noOutlines')}</div>
+      }
+      {outlines.map((outline, i) => (
+        <Outline key={i} outline={outline} isVisible />
+      ))}
+    </div>
+  );
+};
 
 const mapStateToProps = state => ({
   isDisabled: selectors.isElementDisabled(state, 'outlinePanel')
